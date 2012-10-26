334b14e883c117660dbee4bd91ad112a180dd779
Spkordell <mask@mask>
2012-10-26 14:55:36 -0400

    Added Main.Java and Made Many Syntax Corrections

--- HW1/HW1.java
+++ HW1/HW1.java
@@ -11,12 +11,12 @@ Authors:
 //Add Methods
 //And yes, it's one file, but it's under 200 lines, so I think it's fine. If you want to break it up, go for it.
 
-Interface ITournament {}
+interface ITournament {}
 
 //To ensure all score classes have an isValid method, an interface was created containing the method name.
 //All classes related to scoring should implement this interface.
-Interface IScores {
-  isValid();
+interface IScores {
+  boolean isValid();
 }
 class InitMatch implements ITournament {
   MatchData data;
@@ -30,7 +30,7 @@ class AdvanceMatch implements ITournament {
   ITournament feeder2;
   AdvanceMatch (MatchData data,ITournament feeder1,ITournament feeder2) {
     this.data=data;
-    this.feeder1.feeder1;
+    this.feeder1=feeder1;
     this.feeder2=feeder2;
   }
 }
@@ -38,26 +38,26 @@ class MatchData {
   String team1;
   String team2;
   SoccerScore score;
-  matchData(String team1,String team2,SoccerScore score) {
+  MatchData(String team1,String team2,SoccerScore score) {
     this.team1=team1;
     this.team2=team2;
     this.score=score;
   }
 }
-class SoccerScore Implements IScores{
+class SoccerScore implements IScores {
   int goals1;
   int goals2;
   boolean extraTime;
-  soccerScore(int goals1,int goals2,boolean extraTime) {
+  SoccerScore(int goals1,int goals2,boolean extraTime) {
     this.goals1=goals1;
     this.goals2=goals2;
     this.extraTime=extraTime;
   }
-  boolean isValid() {
+  public boolean isValid() {
     return ((this.goals1==this.goals2) && extraTime);
   }
 }
-class BaseballScore Implements IScores{
+class BaseballScore implements IScores{
   int runs1;
   int runs2;
   int totalInnings;
@@ -66,8 +66,8 @@ class BaseballScore Implements IScores{
     this.runs2=runs2;
     this.totalInnings=totalInnings;
   }
-  boolean isValid() {
-    return (totalInnings>=9 && this.runs1!=this.runs2)
+  public boolean isValid() {
+    return (totalInnings>=9 && this.runs1!=this.runs2);
   }
 }
 
